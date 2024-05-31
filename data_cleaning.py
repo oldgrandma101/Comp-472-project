aaccb06c9f6a222f3c328be1f348361f5fbc71c1
oldgrandma101 <adphelps8@gmail.com>
2024-05-31 09:35:21 -0400

    I split up the code so that we're not making a different dataset every time we run the code.

--- data_cleaning.py
+++ data_cleaning.py
@@ -87,18 +87,15 @@ select_random_images("./neutral","./random_neutral",500)
 # #***********************************
 # #***********************************
 # #import from fer datasets, focused dataset, and import group pictures
-fer_happy=import_images("./happy")
-fer_angry=import_images("./angry")
-fer_neutral=import_images("./neutral")
+fer_happy=import_images("./random_happy")
+fer_angry=import_images("./random_angry")
+fer_neutral=import_images("./random_neutral")
 focused = import_images("./random_focused")
 group_happy=import_images("./group_happy")
 group_angry=import_images("./group_angry")
 group_neutral=import_images("./group_neutral")
 group_engaged=import_images("./group_engaged")
 
-
-# #***************************************
-# #***************************************
 # #make list of clean images for each class
 clean_happy=clean_images(fer_happy)
 clean_angry=clean_images(fer_angry)
@@ -122,62 +119,5 @@ export_images("./Final_dataset/final_angry",clean_group_angry)
 export_images("./Final_dataset/final_neutral",clean_group_neutral)
 export_images("./Final_dataset/final_focused",clean_group_focused)
 
-# #import final datsets to be used for project
-list_final_angry=import_images("./Final_dataset/final_angry")
-list_final_happy=import_images("./Final_dataset/final_happy")
-list_final_neutral=import_images("./Final_dataset/final_neutral")
-list_final_focused=import_images("./Final_dataset/final_focused")
 
-# save a 25 image sample from each class to upload on moodle
-select_random_images("./Final_dataset/final_angry","./Samples/angry",25)
-select_random_images("./Final_dataset/final_happy","./Samples/happy",25)
-select_random_images("./Final_dataset/final_neutral","./Samples/neutral",25)
-select_random_images("./Final_dataset/final_focused","./Samples/focused",25)
-
-# #lists of lables for each emotion
-list_angry_labels=label_images(list_final_angry,angry_label)
-list_happy_labels=label_images(list_final_happy,happy_label)
-list_neutral_labels=label_images(list_final_neutral,neutral_label)
-list_focused_labels=label_images(list_final_focused,focused_label)
-
-
-# #convert images in lists to numpy array, they're still stored in a list
-angry_images_as_array = convert_images_to_numpy_array(list_final_angry) #these are lists that contain arrays
-happy_images_as_array = convert_images_to_numpy_array(list_final_happy)
-neutral_images_as_array = convert_images_to_numpy_array(list_final_neutral)
-focused_images_as_array = convert_images_to_numpy_array(list_final_focused)
-
-# #add all lists together to make one big list
-final_list_of_all_images = angry_images_as_array + happy_images_as_array + neutral_images_as_array+focused_images_as_array
-final_list_of_all_labels = list_angry_labels + list_happy_labels + list_neutral_labels+list_focused_labels
-
-
-# #************************************
-# #************************************
-# #Caspar and Jonny, I think you guys are going to need the numpy arrays that I made below this comment
-# #in your data_visualization.py file
-#
-# #these are numpy arrays of each emotion that store each image as a numpy array which stores the pixel values
-
-#************************************
-#************************************
-#Caspar and Jonny, I think you guys are going to need the numpy arays that I made below this comment
-#in your data_visualization.py file
-
-#these are numpy arrays of each emotion that store each image as a numpy array which stores the pixel values
-
-array_of_angry = np.array(angry_images_as_array)
-array_of_happy = np.array(happy_images_as_array)
-array_of_neutral = np.array(neutral_images_as_array)
-array_of_focused = np.array(focused_images_as_array)
-
-# #numpyarrays storing labels for each emotion
-array_angry_labels=np.array(list_angry_labels)
-array_happy_labels=np.array(list_happy_labels)
-array_neutral_labels=np.array(list_neutral_labels)
-array_focused_labels = np.array(list_focused_labels)
-
-# #numpy array containing whole dataset together and all the labels together
-final_array_of_all_images = np.array(final_list_of_all_images)
-final_array_of_all_labels = np.array(final_list_of_all_labels)
 
