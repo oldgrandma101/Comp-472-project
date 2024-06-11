b28611226e1fcd52ffb07207fedab1f78ec40b06
oldgrandma101 <adphelps8@gmail.com>
2024-06-11 17:27:39 -0400

    made two variant CNNs called CNN_2 and CNN_3

--- CNN_3.py
+++ CNN_3.py
@@ -1,14 +1,13 @@
-#this project needs 3 different variants of CNNs to experiment with, this is the first one
-#This variant has 10 convolutional layers with 1 linear layer to classify into the 4 classes
-#I didn't add layers that produce 512 outputs, that could be something to try if this doesn't perform well
+#this project needs 3 different variants of CNNs to experiment with, this is the third one
+#This variant has 14 convolutional layers with 1 linear layer to classify into the 4 classes
 #Can also try different activation functions instead of leakyRELU or different pooling than MaxPool2D
-#can also experiment with the number of convolutional layers
+#can also experiment with the number of convolutional layers and size of kernels and strides
 
 import torch.nn as nn
-class CNN_1(nn.Module):
+class CNN_3(nn.Module):
     #define constructor
     def __init__(self):
-        super(CNN_1, self).__init__()
+        super(CNN_3, self).__init__()
         self.conv_layer = nn.Sequential(    #defines convolutional layers in CNN
 
             #in_channels = 1 because our images are grayscale
@@ -66,13 +65,39 @@ class CNN_1(nn.Module):
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1, stride=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2,stride=2)
+            nn.MaxPool2d(kernel_size=2,stride=2),
+
+            #layer 11 has 512 kernels
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(inplace=True),
+
+            #layer 12 has 512 kernels
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+
+
+            #layer 13 has 1024 kernels
+            nn.Conv2d(in_channels=512, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(inplace=True),
+
+            #layer 14 has 1024 kernels
+            nn.Conv2d(in_channels=1024, out_channels=1024, kernel_size=3, padding=1, stride=1),
+            nn.BatchNorm2d(1024),
+            nn.LeakyReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+
+
+
         )
 
         #fully conected layer
         self.fully_connected_layer = nn.Sequential(
             nn.Dropout(p=0.1),  #this makes 10% of all weights zero, Idea is to avoid becoming too reliant on certain connections and avoid overfitting
-            nn.Linear(7 * 7 * 256, 4)   #4 because we have 4 emotions to classify, floor(244\2^5) = 7
+            nn.Linear(1 * 1 * 1024, 4)   #4 because we have 4 emotions to classify, floor(244\2^7) = 1
                                                     #because each MaxPool2D cuts the size in half
         )
 
