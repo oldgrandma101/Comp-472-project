4ff04088687ab527eefc003db26d941583fd70e7
oldgrandma101 <115679097+oldgrandma101@users.noreply.github.com>
2024-06-27 20:59:14 -0400

    Update updated_training.py

--- updated_training.py
+++ updated_training.py
@@ -61,6 +61,6 @@ def training(model, train_loader, validation_loader, criterion, optimizer, num_e
             patience_count = 0  # reset patience if a new best model is found
         else:
             patience_count += 1
-            if patience_count >= 3:  # patience of 3
+            if patience_count >= 6:  # patience of 3
                 print('Early stopping mechanism activated to avoid over fitting of the model.')
                 break
